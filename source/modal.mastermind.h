765788557751386a214530d690163c07232ab1e9
Thomas Fraiponts <Thomas.Fraiponts@student.uliege.be>
2024-04-16 15:22:19 +0200

    Creation of opaque types and declaration of enumerations necessary for the game.

--- source/modal.mastermind.h
+++ source/modal.mastermind.h
@@ -1,4 +1,24 @@
 #ifndef __MODAL_MASTERMIND__
 #define __MODAL_MASTERMIND__
 
+/**
+ * \brief Defines the different colors of a pawn.
+*/
+typedef enum{
+    RED,    /*!< Red color */
+    BLUE,   /*!< Blue color */
+    GREEN,  /*!< Green color */
+    YELLOW  /*!< Yellow color */
+}COLOR;
+
+/**
+ * Declare the Combination opaque type.
+*/
+typedef struct combination_t Combination;
+
+/**
+ * Declare the Game opaque type. 
+*/
+typedef struct game_t Game;
+
 #endif //__MODAL_MASTERMIND__
